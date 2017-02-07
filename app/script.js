51ed940000e4828b5b13fb3d649662a406b9c4a2
ManeeshAshokan <maneeshashokan@gmail.com>
2017-02-07 22:57:27 +0530

    update

--- app/script.js
+++ app/script.js
@@ -1,3 +1,5 @@
+
+//http://w3lessons.info/2015/03/14/how-to-share-content-on-whatsapp-using-jquery/
 var input = document.getElementById('message_in');
 var msg =document.getElementById('msg-label');
 var maxLength = 50;
@@ -10,3 +12,16 @@ var validateInput = function (event) {
 	// body...
 }
 setInterval(validateInput, 300);
+var out = document.getElementById('out');
+var submit =function  () {
+	
+	console.log(document.getElementById('imgId').value)
+	out.value = document.getElementById('imgId').value;
+}
+
+var forward = function(){
+	var message = encodeURIComponent(out.value);
+	var whatsapp_url = "whatsapp://send?text=" + message;
+	alert(whatsapp_url);
+    window.location.href = whatsapp_url;
+}
